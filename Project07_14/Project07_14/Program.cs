05d54ef7db5ef8de2832f71323716699e0a9c610
27sh <120109008+27sh@users.noreply.github.com>
2023-03-23 10:44:31 +0900

    Add files via upload

---
+++ Project07_14/Project07_14/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project07_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = new char[5];
+            int top = 0;
+
+            s[top] = '1';
+            Console.WriteLine("PUSH : {0}", s[top]);
+            top++;
+
+            s[top] = '2';
+            Console.WriteLine("PUSH : {0}", s[top]);
+            top++;
+
+            s[top] = '3';
+            Console.WriteLine("PUSH : {0}", s[top]);
+            top++;
+
+            Console.WriteLine();
+
+            Console.Write("List : ");
+
+            foreach (char list in s)
+            {
+                Console.Write(list+ " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            top--;
+            Console.WriteLine("POP : {0}", s[top]);
+            s[top] = ' ';
+
+            top--;
+            Console.WriteLine("POP : {0}", s[top]);
+            s[top] = ' ';
+
+            Console.WriteLine();
+
+            Console.Write("List : ");
+
+            foreach (char list in s)
+            {
+                Console.Write(list + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ stack/stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            Console.WriteLine("Push : 1");
+            stack.Push(2);
+            Console.WriteLine("Push : 2");
+            stack.Push(3);
+            Console.WriteLine("Push : 3");
+
+            Console.WriteLine();
+
+            Console.Write("list : ");
+
+            foreach (var str in stack)
+            {
+                Console.Write("{0} ", str);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("POP : {0}", stack.Pop());
+            Console.WriteLine("POP : {0}", stack.Pop());
+
+            Console.WriteLine();
+
+            Console.Write("list : ");
+
+            foreach (var str in stack)
+            {
+                Console.Write("{0}", str);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
